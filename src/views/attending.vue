a64cd7ace943f6e234b4ee2d96f3acd247fa0835
Vrijraj Singh <vrijraj2396@gmail.com>
2019-03-18 15:29:21 +0530

    Attending images updated

--- src/views/attending.vue
+++ src/views/attending.vue
@@ -1,7 +1,7 @@
 <template>
     <v-slide-y-reverse-transition>
         <div>
-        <v-container fluid v-show="show" class="mt-3 blue" style="border-bottom-right-radius:30%;background-size:100%" :style="{'background-image':'url('+require('@/assets/img/bgwtm.jpg')+')'}" >
+        <v-container fluid v-show="show" class="mt-3 blue" style="border-bottom-right-radius:30%;background-size:2000px" :style="{'background-image':'url('+require('@/assets/img/bgwtm.jpg')+')'}" >
             <v-layout wrap align-center justify-center row>
                 <v-flex xs12 md10 lg10>
                     <attnedingHome/>
