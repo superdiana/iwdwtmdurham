d4637f6f3f215ad3e896259f1f647c87c18ed78c
Vrijraj Singh <vrijraj2396@gmail.com>
2019-03-29 11:51:05 +0530

    Update attending.vue

--- src/views/attending.vue
+++ src/views/attending.vue
@@ -1,7 +1,7 @@
 <template>
     <v-slide-y-reverse-transition>
         <div>
-        <v-container fluid v-show="show" class="mt-3 blue" style="border-bottom-right-radius:30%;background-size:2000px" :style="{'background-image':'url('+require('@/assets/img/bgwtm.jpg')+')'}" >
+        <v-container fluid v-show="show" class="mt-3 blue elevation-3" style="border-bottom-right-radius:5%;border-bottom-left-radius:5%;background-size:3000px" :style="{'background-image':'url('+require('@/assets/img/bgwtm.jpg')+')'}" >
             <v-layout wrap align-center justify-center row>
                 <v-flex xs12 md10 lg10>
                     <attnedingHome/>
