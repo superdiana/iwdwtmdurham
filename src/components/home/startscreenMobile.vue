a57613e56d53542370f742c1c513c4b42672432c
Vrijraj Singh <vrijraj2396@gmail.com>
2019-03-29 11:55:09 +0530

    Update startscreenMobile.vue

--- src/components/home/startscreenMobile.vue
+++ src/components/home/startscreenMobile.vue
@@ -18,11 +18,11 @@
                     </v-layout>
                 </v-img>
                 
-                <h1 class="google-font mt-0" style="color:#757575">{{data.eventName}}</h1>
+                <h2 class="google-font mt-0" style="color:#757575">{{data.eventName}}</h2>
 
-                <p class="google-font mb-0" style="font-size:250%;color:#757575;font-weight:800">{{data.themeTitle}}</p>
+                <p class="google-font mb-0" style="font-size:250%;color:#17ACB6;font-weight:800">{{data.themeTitle}}</p>
 
-                <p class="mt-2 google-font" style="font-size:120%;color:#757575">{{data.eventDes}}</p>
+                <p class="mt-2 google-font" style="font-size:120%;">{{data.eventDes}}</p>
 
                 <p class="google-font mb-0" style="font-size:120%;color:#757575">{{data.eventDate}} | {{data.eventTime}} <br> {{data.eventVenue}}</p><br>
                 <p class="google-font mt-1" style="font-size:120%;color:#757575">
@@ -30,8 +30,8 @@
                         {{hs.name}} &nbsp;
                     </span>
                 </p>
-                <v-btn :href="data.registrationLink" target="_blank" class="ma-0 google-font" color="white" style="text-transform: capitalize;border-radius:5px;">Request for an Invitation</v-btn>
-                &nbsp;
+                <!-- <v-btn outline :href="data.registrationLink" target="_blank" class="ma-0 google-font" color="blue" style="text-transform: capitalize;border-radius:5px;">Request for an Invitation</v-btn>
+                &nbsp; -->
                 <v-btn :href="data.eventMeetupLink" target="_blank" round color="blue" style="text-transform: capitalize;border-radius:5px" class="ml-0" dark>Meetup</v-btn>
             
             </v-flex>
